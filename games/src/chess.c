c71846df49779e23230159f6b9ddbc5bb6ab827f
bkliebmann@gmail.com <bkliebmann@gmail.com>
2022-07-16 01:14:54 -0700

    Finished basic chess movegen

--- games/src/chess.c
+++ games/src/chess.c
@@ -32,7 +32,7 @@ typedef enum direction_e {
     D_SW,
     D_W,
     D_NW
-} direction_t;
+} direction_e;
 
 typedef struct piece_t {
     uint8_t color : 1;
@@ -50,6 +50,37 @@ static inline square_t square_up(square_t s, uint8_t n) { return s + n * 8; }
 static inline square_t square_down(square_t s, uint8_t n) { return s - n * 8; }
 static inline square_t square_left(square_t s, uint8_t n) { return s - n; }
 static inline square_t square_right(square_t s, uint8_t n) { return s + n; }
+__attribute__((flatten))
+static inline square_t square_slide(square_t s, uint8_t dir, uint8_t n) {
+    switch(dir) {
+        case D_N: return square_up(s, n);
+        case D_NE: return square_right(square_up(s, n), n);
+        case D_E: return square_right(s, n);
+        case D_SE: return square_right(square_down(s, n), n);
+        case D_S: return square_down(s, n);
+        case D_SW: return square_left(square_down(s, n), n);
+        case D_W: return square_left(s, n);
+        case D_NW: return square_left(square_up(s, n), n);
+        default: return 0;
+    }
+}
+
+static inline bool square_slide_valid(square_t s, uint8_t dir, uint8_t n) {
+    uint8_t x = s % 8;
+    uint8_t y = s / 8;
+    switch(dir) {
+        case D_N: return y + n < 8;
+        case D_NE: return (x + n < 8) && (y + n < 8);
+        case D_E: return x + n < 8;
+        case D_SE: return (x + n < 8) && ((uint8_t)(y - n) < 8);
+        case D_S: return (uint8_t)(y - n) < 8;
+        case D_SW: return ((uint8_t)(x - n) < 8) && ((uint8_t)(y - n) < 8);
+        case D_W: return (uint8_t)(x - n) < 8;
+        case D_NW: return ((uint8_t)(x - n) < 8) && (y + n < 8);
+        default: return 0;
+    }
+}
+
 
 #define INVALID_SQUARE ((uint8_t)255)
 #define IDX(x, y) (((y) * 8) + (x))
@@ -70,7 +101,6 @@ typedef struct move_t {
     bool castle : 1;
     union {
         piece_t promoted;
-        piece_t captured;
         uint8_t castle_side;
     };
 } move_t;
@@ -79,8 +109,8 @@ typedef struct {
     union {
         uint8_t stage;
         struct {
-            uint8_t n : 5;
-            uint8_t direction : 3;
+            uint8_t n : 4;
+            uint8_t direction : 4;
         };
     };
 } movegen_stage_t;
@@ -111,13 +141,14 @@ bool chess_move_iter_drive(chess_move_iter_t *moves) {
     switch(moves->piece.type) {
         case P_PAWN: {
             square_t to;
+            bool up_valid = (uint8_t)(moves->piece.color ? (from / 8 + 1) : (from / 8 - 1)) < 8;
+            square_t up = (moves->piece.color ? square_up(from, 1) : square_down(from, 1));
             switch(moves->stage.stage) {
                 case 0: {
                     moves->stage.stage += 1;
                     if(!moves->piece.moved) {
-                        square_t between = moves->piece.color ? square_up(from, 1) : square_down(from, 1);
                         to = moves->piece.color ? square_up(from, 2) : square_down(from, 2);
-                        if(!moves->board->pieces[to].exists && !moves->board->pieces[between].exists) {
+                        if(!moves->board->pieces[to].exists && !moves->board->pieces[up].exists) {
                             moves->current.to = to;
                             return true;
                         }
@@ -125,19 +156,106 @@ bool chess_move_iter_drive(chess_move_iter_t *moves) {
                 }
                 case 1: {
                     moves->stage.stage += 1;
-                    if((moves->piece.color ? (from / 8 + 1) : (from / 8 - 1)) < 8) {
-                        to = moves->piece.color ? square_up(from, 1) : square_down(from, 1);
-                        if(!moves->board->pieces[to].exists) {
+                    if(up_valid) {
+                        if(!moves->board->pieces[up].exists) {
                             moves->current.from = from;
-                            moves->current.to = to;
-                            break;
+                            moves->current.to = up;
+                            return true;
                         }
+                    }
+                }
+                case 2: {
+                    moves->stage.stage += 1;
+                    to = square_left(from, 1);
+                    if(up_valid && ((uint8_t)(from % 8 - 1) < 8) && moves->board->pieces[to].exists && moves->board->pieces[to].color != moves->piece.color) {
+                        moves->current.to = to;
+                        moves->current.capture = true;
+                        return true;
+                    }
+                }
+                case 3: {
+                    moves->stage.stage += 1;
+                    to = square_right(from, 1);
+                    if(up_valid && ((uint8_t)(from % 8 + 1) < 8) && moves->board->pieces[to].exists && moves->board->pieces[to].color != moves->piece.color) {
+                        moves->current.to = to;
+                        moves->current.capture = true;
+                        return true;
                     }
                 }
             }
         } break;
+        case P_KNIGHT: {
+            square_t to;
+#define ATTACK(n, dx, dy) \
+                case n: { \
+                    moves->stage.stage += 1;   \
+                    uint8_t x = (from % 8) dx; \
+                    uint8_t y = (from / 8) dy; \
+                    to = IDX(x, y);            \
+                    if(x < 8 && y < 8 && (!moves->board->pieces[to].exists || moves->board->pieces[to].color == moves->piece.color)) {     \
+                        moves->current.to = to;                                                                                            \
+                        moves->current.capture = true;                                                                                     \
+                        return true;                                                                                                       \
+                    }                                                                                                                      \
+                }
+            switch(moves->stage.stage) {
+                ATTACK(0, + 2, + 1)
+                ATTACK(1, + 2, - 1)
+                ATTACK(2, + 1, + 2)
+                ATTACK(3, - 1, + 2)
+                ATTACK(4, - 2, + 1)
+                ATTACK(5, - 2, - 1)
+                ATTACK(6, + 1, - 2)
+                ATTACK(7, - 1, - 2)
+            }
+        } break;
+        case P_BISHOP: 
+        case P_ROOK: 
+        case P_QUEEN: {
+            uint8_t step = moves->piece.type == P_QUEEN ? 1 : 2;
+            uint8_t direction = moves->stage.direction;
+            uint8_t n = moves->stage.n;
+            while(direction <= D_NW) {
+                if(square_slide_valid(from, direction, n)) {
+                    square_t to = square_slide(from, direction, n);
+                    if(moves->board->pieces[to].exists) {
+                        if(moves->board->pieces[to].color != moves->piece.color) {
+                            moves->current.to = to;
+                            moves->current.capture = true;
+                            moves->stage.direction = direction;
+                            moves->stage.n = n;
+                            return true;
+                        } else {
+                            direction += step;
+                            n = 0;
+                        }
+                    } else {
+                        moves->current.to = to;
+                        moves->stage.direction = direction;
+                        moves->stage.n = n + 1;
+                        return true;
+                    }
+                } else {
+                    direction += step;
+                    n = 0;
+                }
+           }
+        }
+        case P_KING: {
+            square_t to;
+            switch(moves->stage.stage) {
+                ATTACK(0, + 1, + 0)
+                ATTACK(1, + 1, + 1)
+                ATTACK(2, + 1, - 1)
+                ATTACK(3, + 0, + 1)
+                ATTACK(4, + 0, - 1)
+                ATTACK(5, - 1, + 0)
+                ATTACK(6, - 1, + 1)
+                ATTACK(7, - 1, - 1)
+            }
+        }
     }
-
+#undef ATTACK
     return false;
 }
 
